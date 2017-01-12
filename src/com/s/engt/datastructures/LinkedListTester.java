c45729e735c9a735ccfe4f90a8dff46899316675
Timothy Eng <timothyeng36@gmail.com>
2017-01-12 09:29:48 -0800

    Added testGetSecondLastData() for testing the getSecondLastData() method.

--- src/com/s/engt/datastructures/LinkedListTester.java
+++ src/com/s/engt/datastructures/LinkedListTester.java
@@ -2,7 +2,7 @@
 public class LinkedListTester {
 	
 	//Tests the getLastData() method.
-	//Returns whether the operation succeeds or not.
+	//Returns whether the operation succeeded or not.
 	private static boolean testGetLastData() {
 		LinkedList<String> ll = new LinkedList<String>();
 		if(ll.getLastData() != null) return false;
@@ -10,9 +10,22 @@ public class LinkedListTester {
 		ll.addEnd(data);
 		return ll.getLastData().equals(data);
 	}
+	
+	//Tests the getSecondLastData() method.
+	//Returns whether the operation succeeded or not.
+	private static boolean testGetSecondLastData() {
+		LinkedList<String> ll = new LinkedList<String>();
+		if(ll.getSecondLastData() != null) return false;
+		String data = "wetopuwopaeyuweaopy";
+		ll.addEnd(data);
+		if(ll.getSecondLastData() != null) return false;
+		String data2 = "2306972q9y78@#(*&^(!*@#^UHY(Q@^*BEwehyo2uy";
+		ll.addEnd(data2);
+		return ll.getSecondLastData().equals(data);
+	}
 	public static void main(String[] args) {
 		System.out.println("Testing getLastData() returned " + testGetLastData());
-		
+		System.out.println("Testing getSecondLastData() returned " + testGetSecondLastData());
 	}
 
 }
