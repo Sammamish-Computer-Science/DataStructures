ce199d6182c1e5eed5e65f9665dbabcb2860c947
Timothy Eng <timothyeng36@gmail.com>
2017-01-13 08:46:09 -0800

    Added testGetData() for testing the getData() method.

--- src/com/s/engt/datastructures/LinkedListTester.java
+++ src/com/s/engt/datastructures/LinkedListTester.java
@@ -23,6 +23,31 @@ public class LinkedListTester {
 		ll.addEnd(data2);
 		return ll.getSecondLastData().equals(data);
 	}
+	
+	//Tests the getData() method.
+	//Returns whether the operation succeeded or not.
+	private static boolean testGetData() {
+		LinkedList<String> ll = new LinkedList<String>();
+		try {
+			ll.getData(-1);
+			return false;
+		} catch(IndexOutOfBoundsException e) {}
+		
+		String data = "^(QYOEHsdygw36#w36A%^30AEGH#^3690Y*Q23yY#(#";
+		ll.addEnd(data);
+		if(!ll.getData(0).equals(data)) return false;
+		
+		try {
+			ll.getData(1);
+			return false;
+		} catch(IndexOutOfBoundsException e) {}
+		
+		String data2 = "#W^DGSw3yehs#yuwrwejhD#^*(";
+		ll.addEnd(data2);
+		if(!ll.getData(1).equals(data2)) return false;
+		
+		return true;
+	}
 	public static void main(String[] args) {
 		System.out.println("Testing getLastData() returned " + testGetLastData());
 		System.out.println("Testing getSecondLastData() returned " + testGetSecondLastData());
