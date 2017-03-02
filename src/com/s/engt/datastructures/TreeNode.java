a5526dfcc8db86e7db009fa8b6cabdc3a1709966
Timothy Eng <timothyeng36@gmail.com>
2017-03-02 09:32:33 -0800

    Added getter and setter methods.

--- src/com/s/engt/datastructures/TreeNode.java
+++ src/com/s/engt/datastructures/TreeNode.java
@@ -4,8 +4,26 @@ public class TreeNode<T> {
 	private TreeNode<T> m_left;
 	private TreeNode<T> m_right;
 	public TreeNode(T data, TreeNode<T> left, TreeNode<T> right) {
-		m_data = data;
+		setData(data);
+		setLeft(left);
+		setRight(right);
+	}
+	public void setLeft(TreeNode<T> left) {
 		m_left = left;
+	}
+	public void setRight(TreeNode<T> right) {
 		m_right = right;
+	}
+	public void setData(T data) {
+		m_data = data;
+	}
+	public TreeNode<T> getLeft() {
+		return m_left;
+	}
+	public TreeNode<T> getRight() {
+		return m_right;
+	}
+	public T getData() {
+		return m_data;
 	}
 }
