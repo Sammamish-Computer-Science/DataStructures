faf187f2d512605a9481da3c0b8d8a563c8a1eaf
Timothy Eng <timothyeng36@gmail.com>
2017-03-14 08:46:04 -0700

    Added constructor to QuestionTree.

--- src/com/s/engt/datastructures/20Q/QuestionTree.java
+++ src/com/s/engt/datastructures/20Q/QuestionTree.java
@@ -1,4 +1,9 @@
 
 public class QuestionTree {
-
+	private QuestionNode<String> m_root;
+	
+	//Constructs a question tree with one leaf node representing the object "computer"
+	public QuestionTree() {
+		m_root = new QuestionNode<String>("Computer", null, null);
+	}
 }
