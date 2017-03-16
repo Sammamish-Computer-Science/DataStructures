30555e6526f6c0997db7bab27f22a2ffa55c6ff5
Timothy Eng <timothyeng36@gmail.com>
2017-03-16 09:37:50 -0700

    Added QuestionTree.write() to write a tree to a file.

--- src/com/s/engt/datastructures/20Q/QuestionTree.java
+++ src/com/s/engt/datastructures/20Q/QuestionTree.java
@@ -1,6 +1,5 @@
+import java.io.PrintStream;
 import java.util.Scanner;
-
-import QuestionTree.ProcessNode;
 
 public class QuestionTree {
 	private QuestionNode<String> m_root;
@@ -30,12 +29,6 @@ public class QuestionTree {
 	//Replaces the current tree with a new tree using the information in the file read by the Scanner input
 	public void read(Scanner input) {
 		m_root = readHelper(input);
-		m_root.processTreePreOrder(new ProcessNode(){
-			@Override
-			public void processNode(QuestionNode<?> node) {
-				System.out.println(node.getData());
-			}
-		});
 	}
 	//Recursively reads the input and create a tree (preorder)
 	private QuestionNode<String> readHelper(Scanner input) {
@@ -50,8 +43,30 @@ public class QuestionTree {
 		}
 		return root;
 	}
+	//Writes this tree to a file.
+	public void write(PrintStream input) {
+		m_root.processTreePreOrder(new TreeWriter(input));
+	}
 	//This interface contains one method, processNode, that will be called when the nodes are being processed while a traversal is being performed.
 	public interface ProcessNode {
 		public void processNode(QuestionNode<?> node);
+	}
+	//This class reads the data from each node and writes it to a file
+	private class TreeWriter implements ProcessNode {
+		private PrintStream m_input;
+		private TreeWriter(PrintStream input) {
+			m_input = input;
+		}
+		@Override
+		public void processNode(QuestionNode<?> node) {
+			if(node.getLeft() == null && node.getRight() == null) {
+				//This is an answer node (a leaf)
+				m_input.println("A:");
+			} else {
+				//This is a question node (it has children)
+				m_input.println("Q:");
+			}
+			m_input.println(node.getData());
+		}
 	}
 }
