52797a650af29b2b97949f3520bfc5a666387e74
Timothy Eng <timothyeng36@gmail.com>
2017-03-16 09:10:36 -0700

    Added QuestionTree.read() for creating a tree from a file.

--- src/com/s/engt/datastructures/20Q/QuestionTree.java
+++ src/com/s/engt/datastructures/20Q/QuestionTree.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class QuestionTree {
 	private QuestionNode<String> m_root;
 	private Scanner console;
+	
 	//Constructs a question tree with one leaf node representing the object "computer"
 	public QuestionTree() {
 		m_root = new QuestionNode<String>("Computer", null, null);
@@ -22,5 +23,25 @@ public class QuestionTree {
 			else 
 				System.out.println("Please answer y or n."); 
 		} 
+	}
+	
+	//Replaces the current tree with a new tree using the information in the file read by the Scanner input
+	public void read(Scanner input) {
+		m_root = readHelper(input);
+	}
+	//Recursively reads the input and create a tree (preorder)
+	private QuestionNode<String> readHelper(Scanner input) {
+		//Check for null and read a line from the Scanner
+		if(input == null) return null;
+		String questionType = input.nextLine();
+		String line = input.nextLine();
+		if(questionType == null || line == null) return null;
+		QuestionNode<String> root = new QuestionNode<String>(line, null, null);
+		if(questionType.equals("Q:")) {
+			//This node has children (it's a question), find them
+			root.setLeft(readHelper(input));
+			root.setRight(readHelper(input));
+		}
+		return root;
 	}
 }
